ed762f1b1d66109e2de1c5fe8e71dd237e20ec1f
“hadigun007” <depository.id>
2022-11-04 16:49:20 +0700

    latihan function 3

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 )
 
 func main() {
+	functions()
+}
+
+func functions() {
 	function.Function() // Latihan function
 	var hasilAdd = function.Add(50, 20)
 	fmt.Println(hasilAdd) // 70
@@ -14,6 +18,10 @@ func main() {
 	fmt.Println(hasilSub) // 8
 	var a, b = function.MultVal(5, "value kedua")
 	fmt.Println(a) // 5
-	fmt.Println(b) // value kedua
-
+	fmt.Println(b) // value
+	var age = 20
+	var text = "John"
+	fmt.Println("Before : ", age, text) // Before : 5 John
+	function.Update(&age, &text)
+	fmt.Println("after : ", age, text) // After : 20 John John
 }
